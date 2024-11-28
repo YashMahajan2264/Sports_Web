582673668104ea324192c0c7a80d01c82f1336cf
YashMahajan2264 <yash2264mahajan@gmail.com>
2024-11-28 14:58:44 +0530

    final

--- classes/getUsers.php
+++ classes/getUsers.php
@@ -2,12 +2,12 @@
 
 include_once 'Database.php';
 
-// Create a new database instance
+
 $db = new Database();
 $conn = $db->connect();
 
 try {
-    // Fetch all users
+    
     $query = "SELECT * FROM users";
     $stmt = $conn->prepare($query);
     $stmt->execute();
@@ -15,7 +15,7 @@ try {
     // Fetch data as an associative array
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Return data in JSON format
+    
     echo json_encode($users);
 } catch (PDOException $e) {
     echo json_encode(["error" => $e->getMessage()]);
